729f7bcd4e54bd78e9a9c96b09d3a8037df87aea
itolu <i_tolu@yahoo.com>
2024-11-09 07:22:28 +0100

    :sparkles: Feat: tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  theme: {
+    colors: {
+      'collo-grey-4': '#575A65',
+      'collo-grey-5': '#6E717C'
+    },
+    fontFamily: {
+      'albert': ['Albert Sans']
+    }
+  }
+}
