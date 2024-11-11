db57636c19143e11770ae3d1478e29d3e595f945
itolu <i_tolu@yahoo.com>
2024-11-11 20:17:38 +0100

    :sparkles: Feat: landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,17 @@ import type { Config } from 'tailwindcss'
 export default <Partial<Config>>{
   theme: {
     colors: {
+      'white': '#FFF',
+      'collo-grey': '#F2F4F7',
+      'collo-grey-50': '#F9FAFB',
+      'collo-grey-5': '#6E717C',
       'collo-grey-4': '#575A65',
-      'collo-grey-5': '#6E717C'
+      'collo-heading': '#171717',
+      'collo-text': '#5C5C5C'
     },
     fontFamily: {
-      'albert': ['Albert Sans']
+      'albert': ['Albert Sans'],
+      'inter': ['Inter']
     }
   }
 }
