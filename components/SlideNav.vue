19fb51464c7072533d0c521e6525381d2b6487ab
itolu <i_tolu@yahoo.com>
2024-11-12 13:55:26 +0100

    :sparkles: Feat: complete about us page

---
+++ components/SlideNav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex gap-6 sm:gap-8 justify-end right-0 bottom-0">
+        <div class="flex items-center justify-center rounded-full border-[0.583px] sm:border border-[#ABADB2] bg-[rgba(255,255,255,0.90)] w-[32.667px] sm:w-14 h-[32.667px] sm:h-14">
+            <svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" fill="none" class="w-3.5 sm:w-6 h-3.5 sm:h-6">
+                <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="#7D7F85" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+        <div class="flex items-center justify-center rounded-full border-[0.583px] sm:border border-[#ABADB2] bg-[rgba(255,255,255,0.90)] w-[32.667px] sm:w-14 h-[32.667px] sm:h-14">
+            <svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" fill="none" class="w-3.5 sm:w-6 h-3.5 sm:h-6">
+                <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="#7D7F85" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,9 @@
         <AsFeaturedIn />
         <section class="bg-white pb-24 sm:py-16 px-5 sm:px-20">
             <div class="text-center">
-                <p class="mb-3 text-[#631CBF] font-inter font-semibold leading-6">Services</p>
+                <p class="mb-3 text-[#631CBF] font-inter font-semibold leading-6">
+                    Services
+                </p>
                 <p class="mb-3 sm:mb-5 text-collo-heading text-2xl sm:text-[40px] font-semibold leading-[normal] sm:leading-[50px]">
                     Collo offers you the best
                 </p>
@@ -226,18 +228,7 @@
                         <p class="text-[#333] text-lg sm:text-2xl font-semibold leading-[normal] sm:leading-[31px]">—Susan Odewale</p>
                     </div>
                 </div>
-                <div class="flex gap-6 sm:gap-8 justify-end sm:absolute right-0 bottom-0">
-                    <div class="flex items-center justify-center rounded-full border-[0.583px] sm:border border-[#ABADB2] bg-[rgba(255,255,255,0.90)] w-[32.667px] sm:w-14 h-[32.667px] sm:h-14">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" fill="none" class="w-3.5 sm:w-6 h-3.5 sm:h-6">
-                            <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="#7D7F85" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </div>
-                    <div class="flex items-center justify-center rounded-full border-[0.583px] sm:border border-[#ABADB2] bg-[rgba(255,255,255,0.90)] w-[32.667px] sm:w-14 h-[32.667px] sm:h-14">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="" height="" viewBox="0 0 24 24" fill="none" class="w-3.5 sm:w-6 h-3.5 sm:h-6">
-                            <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="#7D7F85" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </div>
-                </div>
+                <SlideNav class="sm:absolute" />
             </div>
         </section>
         <section class="bg-white px-5 sm:px-[121px] pb-24 sm:py-24">
