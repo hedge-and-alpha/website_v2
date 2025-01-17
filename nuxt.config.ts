ae37fd2e0e3a0894ea7bed3ab8dae943cd01c56f
itolu <i_tolu@yahoo.com>
2025-01-17 15:54:05 +0100

    :sparkles: Feat: mgr join info page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      mgrBaseURL: process.env.MGR_BASE_URL || "https://app.colloafrica.com/mgr",
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
