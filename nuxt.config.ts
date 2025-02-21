64354afded912c4da966e3ec4ecd7901c25ca3d9
itolu <i_tolu@yahoo.com>
2025-02-21 06:47:25 +0100

    :sparkles: Feat: Google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/scripts'],
+  scripts: {
+    registry: {
+      googleAnalytics: {
+        id: 'G-KEWLN4BNV5',
+      }
+    }
+  }
 })
