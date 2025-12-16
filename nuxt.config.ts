e8b9da0f95b702cf444f6126e9a71b7faafb5098
Daniel <lekanm715@gmail.com>
2025-12-16 08:18:00 +0100

    fixed configuration error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,19 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      mgrBaseURL: process.env.MGR_BASE_URL || "https://app.colloafrica.com/mgr",
-      apiBaseURL: process.env.API_BASE_URL || "https://live-server.colloafrica.com/api/v1",
-    }
+      mgrBaseURL: "https://app.colloafrica.com/mgr",
+      apiBaseURL: "https://live-server.colloafrica.com/api/v1",
+    },
   },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/scripts'],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/scripts"],
   scripts: {
     registry: {
       googleAnalytics: {
-        id: 'G-KEWLN4BNV5',
-      }
-    }
-  }
-})
+        id: "G-KEWLN4BNV5",
+      },
+    },
+  },
+});
